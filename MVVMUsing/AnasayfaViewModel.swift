a17c4c3107cd8666a4b6381fc6cf9a0aab954d58
atilla.citirik <atillactrk@gmail.com>
2024-05-23 16:50:24 +0300

    Creating MVVM structure and RXSwift

--- MVVMUsing/AnasayfaViewModel.swift
+++ MVVMUsing/AnasayfaViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import RxSwift
+
+class AnasayfaViewModel {
+    
+    var sonuc = BehaviorSubject<String>(value: "0")
+    
+    func toplamaYap(alinanSayi1:String, alinanSayi2:String) {
+        
+        if let sayi1 = Int(alinanSayi1), let sayi2 = Int(alinanSayi2){
+            let toplam = sayi1 + sayi2
+            sonuc.onNext(String(toplam))
+        }
+        
+    }
+    
+    func carpmaYap(alinanSayi1:String, alinanSayi2:String) {
+       
+        if let sayi1 = Int(alinanSayi1), let sayi2 = Int(alinanSayi2){
+            let carpma = sayi1 * sayi2
+            sonuc.onNext(String(carpma))
+        }
+        
+    }
+    
+    
+}
--- MVVMUsing/ViewController.swift
+++ MVVMUsing/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelSonuc: UILabel!
+    @IBOutlet weak var tfSayi1: UITextField!
+    @IBOutlet weak var tfSayi2: UITextField!
+    
+    var viewModel = AnasayfaViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        _ = viewModel.sonuc.subscribe(onNext: { s in
+            self.labelSonuc.text = s
+            
+        })
+        
     }
 
-
+    @IBAction func buttonToplama(_ sender: Any) {
+        if let alinanSayi1 = tfSayi1.text, let alinanSayi2 = tfSayi2.text {
+            viewModel.toplamaYap(alinanSayi1: alinanSayi1, alinanSayi2: alinanSayi2)
+        }
+    }
+    
+    @IBAction func buttonCarpma(_ sender: Any) {
+        if let alinanSayi1 = tfSayi1.text, let alinanSayi2 = tfSayi2.text {
+            viewModel.carpmaYap(alinanSayi1: alinanSayi1, alinanSayi2: alinanSayi2)
+        }
+    }
+    
 }
 
