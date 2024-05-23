e294b1a3c48ea0cf90fe751c5252e06731add401
atilla.citirik <atillactrk@gmail.com>
2024-05-23 16:54:01 +0300

    Repository creation and use

---
+++ MVVMUsing/MatematikRepository.swift
@@ -0,0 +1,33 @@
+//
+//  MatematikRepository.swift
+//  MVVMUsing
+//
+//  Created by Atilla Çıtırık on 23.05.2024.
+//
+
+import Foundation
+import RxSwift
+
+class MatematikRepository {
+ 
+    var sonuc = BehaviorSubject<String>(value: "0")
+    
+    func toplamaYap(alinanSayi1:String, alinanSayi2:String) {
+        
+        if let sayi1 = Int(alinanSayi1), let sayi2 = Int(alinanSayi2){
+            let toplam = sayi1 + sayi2
+            sonuc.onNext(String(toplam))
+        }
+        
+    }
+    
+    func carpmaYap(alinanSayi1:String, alinanSayi2:String) {
+       
+        if let sayi1 = Int(alinanSayi1), let sayi2 = Int(alinanSayi2){
+            let carpma = sayi1 * sayi2
+            sonuc.onNext(String(carpma))
+        }
+        
+    }
+    
+}
